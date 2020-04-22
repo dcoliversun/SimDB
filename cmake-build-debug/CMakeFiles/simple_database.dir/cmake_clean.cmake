5359a5ee446b77bae3dd4cfa7dc65eaab8726912
OliverSun <sunqian18s@ict.ac.cn>
2020-04-22 23:13:29 +0800

    update db.c and CMakefile

---
+++ cmake-build-debug/CMakeFiles/simple_database.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_database.dir/db.c.o"
+  "simple_database"
+  "simple_database.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
