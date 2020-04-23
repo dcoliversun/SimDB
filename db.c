706bc62c95d349d50d0e02656a893826479f4b26
OliverSun <sunqian18s@ict.ac.cn>
2020-04-23 21:30:22 +0800

    define function new_table/free_table

--- db.c
+++ db.c
@@ -40,6 +40,22 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+Table* new_table() {
+    Table* table = malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; ++i) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table) {
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; ++i) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
 void* row_slot(Table* table, uint32_t row_num) {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
     void* page = table->pages[page_num];
