1cbba3eebb5165cd71b54803e40e509b43c55791
OliverSun <sunqian18s@ict.ac.cn>
2020-04-23 20:50:34 +0800

    update prepare_statement

--- db.c
+++ db.c
@@ -97,6 +97,11 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+        int args_assign = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                                 statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assign < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
