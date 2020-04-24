e1a015cf7b5e357efbf91b8355c0ee3b9e700ca3
OliverSun <sunqian18s@ict.ac.cn>
2020-04-24 14:43:14 +0800

    更新 serialized_row，将memcpy转为strncpy，避免不同机器大小端问题

--- db.c
+++ db.c
@@ -192,8 +192,8 @@ void* row_slot(Table* table, uint32_t row_num) {
 
 void serialize_row(Row* source, void* destination) {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_OFFSET);
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 void deserialize_row(void* source, Row* destination) {
