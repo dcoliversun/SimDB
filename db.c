c167bc895a0d35c9054a9ddd64ac6223bb0f9293
OliverSun <sunqian18s@ict.ac.cn>
2020-04-24 14:45:36 +0800

    更新main函数，添加文件名输入

--- db.c
+++ db.c
@@ -341,7 +341,12 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
 }
 
 int main(int argc, char** argv) {
-    Table* table = new_table();
+    if (argc < 2) {
+        printf("Must supply a database filename.\n");
+        exit(EXIT_FAILURE);
+    }
+    char* filename = argv[1];
+    Table* table = db_open(filename);
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
