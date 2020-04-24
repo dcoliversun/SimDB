c3f156ad73e506299a227d40f34bfeb4f133a0ed
OliverSun <sunqian18s@ict.ac.cn>
2020-04-24 08:41:06 +0800

    update do_meta_command

--- db.c
+++ db.c
@@ -199,7 +199,7 @@ int main(int argc, char** argv) {
         read_input(input_buffer);
 
         if (input_buffer->buffer[0] == '.') {
-            switch (do_meta_command(input_buffer)) {
+            switch (do_meta_command(input_buffer, table)) {
                 case (META_COMMAND_SUCCESS):
                     continue;
                 case (META_COMMAND_UNRECOGNIZED_COMMAND):
