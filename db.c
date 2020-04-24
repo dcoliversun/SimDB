51b61fc53b8bd01f701200e062d80a6ee850b40b
OliverSun <sunqian18s@ict.ac.cn>
2020-04-24 10:16:59 +0800

    修改 ID 为负数bug

--- db.c
+++ db.c
@@ -120,6 +120,7 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
     PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
@@ -157,6 +158,9 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
     }
 
     int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
 
     if (strlen(username) > COLUMN_USERNAME_SIZE) {
         return PREPARE_STRING_TOO_LONG;
@@ -236,6 +240,9 @@ int main(int argc, char** argv) {
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
             case (PREPARE_STRING_TOO_LONG):
                 printf("String is too long.\n");
                 continue;
