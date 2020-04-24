f21ef81aed81b29c62f995aa4d5d0afa1a9a3984
OliverSun <sunqian18s@ict.ac.cn>
2020-04-24 13:43:53 +0800

    update do_meta

--- db.c
+++ db.c
@@ -259,8 +259,7 @@ typedef struct {
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        close_input_buffer(input_buffer);
-        free_table(table);
+        db_close(table);
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
