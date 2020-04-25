6d4e3782cfca17a2bec93b2721e7dcb7b8eaf350
OliverSun <sunqian18s@ict.ac.cn>
2020-04-25 19:37:26 +0800

    change row_slot to cursor_value

--- db.c
+++ db.c
@@ -204,9 +204,10 @@ void* get_page(Pager* pager, uint32_t page_num) {
     return pager->pages[page_num];
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
+void* cursor_value(Cursor* cursor) {
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
