91bf79532d588a290d387b20679f2b5d913d26ae
OliverSun <sunqian18s@ict.ac.cn>
2020-04-26 21:28:27 +0800

    define function leaf_node_insert

--- db.c
+++ db.c
@@ -287,6 +287,26 @@ void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
+    void* node =get_page(cursor->table->pager, cursor->page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    if (num_cells >= LEAF_NODE_MAX_CELLS) {
+        printf("Need to implement splitting a leaf node.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cursor->cell_num < num_cells) {
+        // Make room for new cell
+        for (uint32_t i = num_cells; i >= cursor->cell_num; --i) {
+            memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1), LEAF_NODE_CELL_SIZE);
+        }
+    }
+
+    *(leaf_node_num_cells(node)) += 1;
+    *(leaf_node_key(node, cursor->cell_num)) = key;
+    serialize_row(value, leaf_node_value(node, cursor->cell_num));
+}
+
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
