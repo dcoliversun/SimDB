11537ae0403dffa2b647c1a002a92b3d6eeaa8c9
OliverSun <sunqian18s@ict.ac.cn>
2020-04-26 21:29:20 +0800

    添加输入状态

--- db.c
+++ db.c
@@ -374,6 +374,10 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         db_close(table);
         exit(EXIT_SUCCESS);
+    } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+        printf("Constants:\n");
+        print_constants();
+        return META_COMMAND_SUCCESS;
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
