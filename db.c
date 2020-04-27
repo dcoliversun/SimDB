b19b19a0ec5b7914b4b6d364b8d7a37a8d6ed07e
OliverSun <sunqian18s@ict.ac.cn>
2020-04-27 14:37:07 +0800

    delete table_end and define table_find

--- db.c
+++ db.c
@@ -147,15 +147,20 @@ Cursor* table_start(Table* table) {
     return cursor;
 }
 
-Cursor* table_end(Table* table) {
-    Cursor* cursor = malloc(sizeof(Cursor));
-    cursor->table = table;
-    cursor->page_num = table->root_page_num;
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
-    cursor->cell_num = num_cells;
-    cursor->end_of_table = true;
-    return cursor;
+/*
+ * Return the position of the given key.
+ * If the key is not present, return the position
+ * where it should be inserted
+ */
+Cursor* table_find(Table* table, uint32_t key) {
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+    if (get_node_type(root_node) == NODE_LEAF) {
+        return leaf_node_find(table, root_page_num, key);
+    } else {
+        printf("Need to implement searching an internal node\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 Pager* pager_open(const char* filename) {
