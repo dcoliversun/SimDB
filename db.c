c66c46f29b452e3e069ef9029855802de191d68d
OliverSun <sunqian18s@ict.ac.cn>
2020-04-27 14:37:26 +0800

    增加输入命令情况

--- db.c
+++ db.c
@@ -389,6 +389,10 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         db_close(table);
         exit(EXIT_SUCCESS);
+    } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+        printf("Tree:\n");
+        print_leaf_node(get_page(table->pager, 0));
+        return META_COMMAND_SUCCESS;
     } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
         printf("Constants:\n");
         print_constants();
