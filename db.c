d5524259dce0f9f52dd4c180e57ce442a4daa773
OliverSun <sunqian18s@ict.ac.cn>
2020-04-27 17:06:49 +0800

    update main

--- db.c
+++ db.c
@@ -574,6 +574,9 @@ int main(int argc, char** argv) {
             case (EXECUTE_SUCCESS):
                 printf("Executed.\n");
                 break;
+            case (EXECUTE_DUPLICATE_KEY):
+                printf("Error: Duplicate key.\n");
+                break;
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
                 break;
