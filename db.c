abcc3e2e808450698ceb0213c87fe100c9e7c19e
OliverSun <sunqian18s@ict.ac.cn>
2020-04-28 22:28:35 +0800

    update execute_insert

--- db.c
+++ db.c
@@ -559,9 +559,6 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 ExecuteResult execute_insert(Statement* statement, Table* table) {
     void* node = get_page(table->pager, table->root_page_num);
     uint32_t num_cells = (*leaf_node_num_cells(node));
-    if (num_cells >= LEAF_NODE_MAX_CELLS) {
-        return EXECUTE_TABLE_FULL;
-    }
 
     Row* row_to_insert = &(statement->row_to_insert);
     uint32_t key_to_insert = row_to_insert->id;
