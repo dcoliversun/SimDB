ded3fe2ef5f1dc263bee7ec91772133b5a0ace57
OliverSun <sunqian18s@ict.ac.cn>
2020-05-01 00:00:02 +0800

    define leaf_node_next_leaf

--- db.c
+++ db.c
@@ -102,6 +102,10 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
     return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+uint32_t * leaf_node_next_leaf(void* node) {
+    return node + LEAF_NODE_NEXT_LEAF_OFFSET;
+}
+
 uint32_t* internal_node_num_keys(void* node) {
     return node + INTERNAL_NODE_NUM_KEYS_OFFSET;
 }
