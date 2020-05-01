d45f5170d36abf405859d1fbb5e6e0d3acd7865a
OliverSun <sunqian18s@ict.ac.cn>
2020-05-01 16:56:27 +0800

    update cursor_advance

--- db.c
+++ db.c
@@ -425,9 +425,18 @@ void* cursor_value(Cursor* cursor) {
 void cursor_advance(Cursor* cursor) {
     uint32_t page_num = cursor->page_num;
     void* node = get_page(cursor->table->pager, page_num);
+
     cursor->cell_num += 1;
     if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
-        cursor->end_of_table = true;
+        /* Advance to next leaf node */
+        uint32_t next_page_num = *leaf_node_next_leaf(node);
+        if (next_page_num == 0) {
+            /* This was rightmost leaf */
+            cursor->end_of_table = true;
+        } else {
+            cursor->page_num = next_page_num;
+            cursor->cell_num = 0;
+        }
     }
 }
 
