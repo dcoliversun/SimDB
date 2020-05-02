c668443a6e149ef512d2b70310d7aa4115f20fb7
OliverSun <sunqian18s@ict.ac.cn>
2020-05-02 21:44:12 +0800

    define function internal_node_insert

--- db.c
+++ db.c
@@ -498,6 +498,43 @@ void update_internal_node_key(void* node, uint32_t old_key, uint32_t new_key) {
     *internal_node_key(node, old_child_index) = new_key;
 }
 
+void internal_node_insert(Table* table, uint32_t parent_page_num, uint32_t child_page_num) {
+
+    /*
+     * Add a new child/key pair to parent that corresponds to child
+     */
+
+    void* parent = get_page(table->pager, parent_page_num);
+    void* child = get_page(table->pager, child_page_num);
+    uint32_t child_max_key = get_node_max_key(child);
+    uint32_t index = internal_node_find_child(parent, child_max_key);
+
+    uint32_t original_num_keys = *internal_node_num_keys(parent);
+    *internal_node_num_keys(parent) = original_num_keys + 1;
+
+    if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
+        printf("Need to implement splitting internal node\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint32_t right_child_page_num = *internal_node_right_child(parent);
+    void* right_child = get_page(table->pager, right_child_page_num);
+
+    if (child_max_key > get_node_max_key(right_child)) {
+        /* Replace right child */
+        *internal_node_child(parent, original_num_keys) = right_child_page_num;
+        *internal_node_key(parent, original_num_keys) = get_node_max_key(right_child);
+        *internal_node_right_child(parent) = child_page_num;
+    } else {
+        /* Make room for the new cell */
+        for (uint32_t i = original_num_keys; i > index; i--) {
+            void* destination = internal_node_cell(parent, i);
+            void* source = internal_node_cell(parent, i - 1);
+            memcpy(destination, source, INTERNAL_NODE_CELL_SIZE);
+        }
+        *internal_node_child(parent, index) = child_page_num;
+        *internal_node_key(parent, index) = child_max_key;
+    }
 }
 
 void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
